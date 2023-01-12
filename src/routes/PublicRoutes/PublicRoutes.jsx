e0db484e298d0db77f0224456aaeb6d10934014d
Alexander Mendivil <alexanderima@outlook.es>
2023-01-11 22:23:57 -0700

    public routes siempre

--- src/routes/PublicRoutes/PublicRoutes.jsx
+++ src/routes/PublicRoutes/PublicRoutes.jsx
@@ -2,6 +2,7 @@ import { Navigate } from "react-router-dom"
 
 export const PublicRoutes = ( { children } ) => {
 
+    localStorage.setItem('user-lab', {id: 1, name: 'admin', password: '123'})
     const user = localStorage.getItem('user-lab');
   return ( user ) ? <Navigate to='/'/> : children;
 }
